37724f844e64a0be1ad3f9cf7b6406f00aba851b
Emmanuel <epmnzava@gmail.com>
2022-08-16 22:58:54 +0300

    updated code

--- src/Pesapal.php
+++ src/Pesapal.php
@@ -15,6 +15,8 @@ class Pesapal
     private $endpoint;
     private $currency;
     private $callback;
+    private $ipn_id;
+    private $ipn_url;
 
     private string $amount,
         $firstname,
@@ -27,7 +29,7 @@ class Pesapal
         $post_xml;
 
 
-    public function __construct($key, $secret, $endpoint, $currency, $callback, $token = null)
+    public function __construct($key, $secret, $endpoint, $currency, $callback, $ipn_url = null, $token = null)
     {
 
         $this->key = $key;
@@ -36,6 +38,139 @@ class Pesapal
         $this->currency = $currency;
         $this->callback = $callback;
         $this->token = $token;
+        $this->ipn_url = $ipn_url;
+    }
+
+    public function getToken()
+    {
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $this->endpoint . '/api/Auth/RequestToken',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => '{
+          "consumer_key": "' . $this->key . '",
+          "consumer_secret": "' . $this->secret . '"
+        }',
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'Accept: application/json'
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        $this->token = json_decode($response)->token;
+        return $this->token;
+    }
+
+    public function registerIpn($ipn_url)
+    {
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $this->endpoint . '/api/URLSetup/RegisterIPN',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => '{
+    "url": "' . $ipn_url . '",
+    "ipn_notification_type": "GET"
+}
+',
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer ' . $this->token,
+                'Content-Type: application/json',
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        $this->ipn_id = json_decode($response)->ipn_id;
+
+        return $this->ipn_id;
+    }
+
+
+    public function submitOrder($firstname, $lastname, $phone_number, $email, $amount, $description, $reference, $type = "MERCHANT")
+    {
+        $this->getToken();
+        $this->registerIpn($this->ipn_url);
+
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+        $this->email = $email;
+        $this->phone_number = $phone_number;
+        $this->type = $type;
+        $this->description = $description;
+        $this->reference = $reference;
+        $this->amount = $amount;
+
+        $token = $this->token;
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $this->endpoint . '/api/Transactions/SubmitOrderRequest',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => '
+{
+    "id": "' . $reference . '",
+    "currency": "' . $this->currency . '",
+    "amount": ' . $amount . ',
+    "description": "' . $description . '",
+    "callback_url": "' . $this->callback . '",
+    "notification_id": "' . $this->ipn_id . '",
+    "billing_address": {
+        "email_address": "' . $email . '",
+        "phone_number": "",
+        "country_code": "",
+        "first_name": "' . $firstname . '",
+        "middle_name": "",
+        "last_name": "' . $lastname . '",
+        "line_1": "",
+        "line_2": "",
+        "city": "",
+        "state": "",
+        "postal_code": "",
+        "zip_code": ""
+    }
+}
+',
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer ' . $this->token,
+                'Content-Type: application/json',
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        $res = json_encode($response);
+
+
+        return $res;
     }
 
     public function processpayment($firstname, $lastname, $phone_number, $email, $amount, $description, $reference, $type = "MERCHANT")
